effea9b2f34901ccf00be0a398dffb1716559704
segoSambel <habibhusain313@gmail.com>
2023-05-26 07:40:21 +0700

    Reorganize: Adapter moved to the UI package for better code structure

--- app/src/main/java/com/jualin/apps/ui/fragments/NearbyFragment.kt
+++ app/src/main/java/com/jualin/apps/ui/fragments/NearbyFragment.kt
@@ -8,7 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.jualin.apps.R
-import com.jualin.apps.adapter.NearbyPagerAdapter
+import com.jualin.apps.ui.adapter.NearbyPagerAdapter
 import com.jualin.apps.databinding.FragmentNearbyBinding
 
 class NearbyFragment : Fragment() {
