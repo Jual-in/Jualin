aeb41b590f52bf5cc31150c9827a06c48f1c3cc0
segoSambel <habibhusain313@gmail.com>
2023-05-30 11:00:50 +0700

    Add: Business detail page along with its corresponding XML

---
+++ app/src/main/java/com/jualin/apps/ui/fragments/BusinessDetailFragment.kt
@@ -0,0 +1,18 @@
+package com.jualin.apps.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jualin.apps.R
+
+class BusinessDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_business_detail, container, false)
+    }
+}
