1b39e69e4a4bc58e2d1afa37069d50d0989bd188
segoSambel <habibhusain313@gmail.com>
2023-06-01 19:17:47 +0700

    Add: Nearby service page

--- app/src/main/java/com/jualin/apps/ui/fragments/NearbyServiceFragment.kt
+++ app/src/main/java/com/jualin/apps/ui/fragments/NearbyServiceFragment.kt
@@ -5,15 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jualin.apps.R
+import com.jualin.apps.databinding.FragmentNearbyServiceBinding
+import com.jualin.apps.ui.adapter.NearbyServiceAdapter
+import com.jualin.apps.utils.FakeData
 
 class NearbyServiceFragment : Fragment() {
+
+    private var _binding: FragmentNearbyServiceBinding? = null
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nearby_service, container, false)
+    ): View {
+        _binding = FragmentNearbyServiceBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvNearbyService.text = getString(R.string.jasa_di_sekitar_s, "Ngawi")
+        binding.rvNearbyService.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvNearbyService.adapter = NearbyServiceAdapter(FakeData.services, requireContext())
     }
 }
