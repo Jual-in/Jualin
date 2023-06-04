e0bc859792664f6b507a24caf224f8f364c2c215
segoSambel <habibhusain313@gmail.com>
2023-06-04 10:34:48 +0700

    Refactor: Update Nearby Fragment page content to utilize the same layout

---
+++ app/src/main/java/com/jualin/apps/ui/adapter/NearbyBusinessAdapter.kt
@@ -0,0 +1,52 @@
+package com.jualin.apps.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.jualin.apps.data.local.entity.Business
+import com.jualin.apps.databinding.ItemNearbyBusinessBinding
+
+class NearbyBusinessAdapter(
+    private val items: List<Business>,
+    private val listener: OnBusinessClickListener
+) :
+    RecyclerView.Adapter<NearbyBusinessAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemNearbyBusinessBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+        holder.bind(item)
+    }
+
+    inner class ViewHolder(private val binding: ItemNearbyBusinessBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Business) {
+            binding.apply {
+                tvBusinessName.text = item.name
+                tvBusinessDesc.text = item.description
+                Glide.with(itemView.context)
+                    .load(item.imageUrl)
+                    .into(ivBusiness)
+            }
+            itemView.setOnClickListener {
+                listener.onBusinessClick(item.id)
+            }
+        }
+
+    }
+
+    interface OnBusinessClickListener {
+        fun onBusinessClick(businessId: Int)
+    }
+}
