5132d9e7c3dc1d0fb85c563269339e5bb9d7958c
Baharudin Dayat <55477637+baharudindayat@users.noreply.github.com>
2023-06-05 22:37:00 +0700

    add fragment umkm and fix navigation

---
+++ app/src/main/java/com/jualin/apps/ui/fragments/AddUmkmFragment.kt
@@ -0,0 +1,30 @@
+package com.jualin.apps.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jualin.apps.databinding.FragmentUmkmBinding
+
+class AddUmkmFragment : Fragment(){
+    private var _binding: FragmentUmkmBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentUmkmBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
